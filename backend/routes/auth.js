95b7bf082d099f62442ea609f6c6e00acb7e12d6
andredc7694 <andre.delcastillo.27@unsch.edu.pe>
2026-06-26 13:54:26 -0500

    HU-005: Middleware de protección JWT implementado y validado

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+const authMiddleware = require('../middlewares/authMiddleware');
+router.get('/me', authMiddleware, (req, res) => {
+    res.json({ success: true, user: req.user });
+});
+
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
